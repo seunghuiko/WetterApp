318934cbcb758600b1f1db80ea15f5bb2cf9ae91
seunghuiko <118679170+seunghuiko@users.noreply.github.com>
2023-01-28 00:35:51 +0100

    upload
    completed

---
+++ WeatherDemo/Views/WelcomeView.swift
@@ -0,0 +1,41 @@
+//
+//  WelcomeView.swift
+//  WeatherDemo
+//
+//  Created by Seunghui Ko on 16.01.23.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Seunghuis Selbstgemachte Wetter App")
+                    .bold().font(.title)
+                
+                Text("Share your Location")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Seunghui Ko on 16.01.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
